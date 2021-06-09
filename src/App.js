afea2c0f4410ca09e0a8019c7e7abb10798165ee
Peter <pete225848@gmail.com>
2021-06-09 08:48:10 -0700

    Finished react, need to CSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,77 @@
+import React, { Component } from 'react'
 import './App.css';
 import Header from './Header.js';
 import ImageList from './ImageList.js';
-import images from './data.js';
+import data from './data.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <ImageList data={images} />
-    </div>
-  );
+export default class App extends Component {
+  
+  state = {
+    filterKeyword: '',
+    filterHorns: ''
+  }
+
+  dataKeyword = (e) => {
+    this.setState({ filterKeyword: e.target.value })
+  }
+  dataHorns = (e)=> {
+    this.setState({ filterHorns: e.target.value })
+  }
+
+  render() {
+    let filteredData = data;
+
+    if (this.state.filterKeyword) {
+      filteredData = data.filter(item => item.keyword === this.state.filterKeyword)
+    }
+    if (this.state.filterHorns) {
+      filteredData = data.filter(item => item.horns === +(this.state.filterHorns))
+    }
+
+    return (
+      <div className="App">
+        <Header />
+        <label> Sort by number of horns!
+        <select onChange={this.dataHorns}>
+          <option value="">View All</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        </label>
+        <label> Sort by creature type!
+        <select onChange={this.dataKeyword}>
+          <option value="">View All</option>
+          <option value="narwhal">Narwhal</option>
+          <option value="rhino">Rhino</option>
+          <option value="unicorn">Unicorn</option>
+          <option value="unilego">Unilego</option>
+          <option value="triceratops">Triceratops</option>
+          <option value="markhor">Markhor</option>
+          <option value="mouflon">Mouflon</option>
+          <option value="addax">Addax</option>
+          <option value="chameleon">Chameleon</option>
+          <option value="lizard">Lizard</option>
+          <option value="dragon">Dragon</option>
+        </select>
+        </label>
+        <ImageList data={filteredData} />
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+// export default class App extends Component {
+//     render() {
+//         return (
+//     <div className="App">
+//       <Header />
+//       <ImageList data={images} />
+//     </div>
+//   ) 
+// }
+// }
+
+// // export default App;
